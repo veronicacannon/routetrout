d9ab067e33e6f5eb7f24c4e3db43554691e9ae2f
Veronica Cannon <me@veronicacannon.com>
2014-07-09 16:41:43 -0700

    Separated json example from accordian.

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def daily_route():
 	html = render_template("accordian.html")
 	return html
 
+@app.route('/json')
+def json_example():
+	html = render_template("json.html")
+	return html
+
 if __name__ == "__main__":
 	app.run(debug = True)
