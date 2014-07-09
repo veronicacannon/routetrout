9ab010f85594651fd9d580ffa3e62b788fdb4989
Veronica Cannon <me@veronicacannon.com>
2014-07-09 14:53:19 -0700

    Ready to preview test on mobile.

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return 'route trout'
+def daily_route():
+	html = render_template("accordian.html")
+	return html
+
+if __name__ == "__main__":
+	app.run(debug = True)
