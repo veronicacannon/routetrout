5f06f03887901c3ad28bacdf0dba6afe25b24f12
Hackbright Student <info@hackbrightacademy.com>
2014-07-14 16:56:29 -0700

    Corrections to refresh and sort health alert list after add, delete.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect, request, flash, session as b_session
 import model
 import os
+import json
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03")
-print app.secret_key
 
 @app.route('/')
 def daily_route():
@@ -33,7 +33,14 @@ def get_health_alert():
     model.session.add(new_health_alert)
     model.session.commit()
     
-    return str(new_health_alert.id)
+    if new_health_alert == None:
+        #send and error
+        return json.JSONEncoder().encode({"error": "Unable to set record"})
+    else:
+        return json.JSONEncoder().encode({
+            "id": str(new_health_alert.id),
+            "desc": new_health_alert.description
+            })
 
 @app.route('/delete_health_alert', methods=["POST"])
 def delete_health_alert():
