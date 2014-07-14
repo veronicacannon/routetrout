d6c4a7455abe191ba0c106d353b794d4229b677c
Veronica Cannon <me@veronicacannon.com>
2014-07-14 10:46:49 -0700

    Updated to point adding and deleting health alerts.

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
-import os
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, request, flash, session as b_session
+import model
 
+app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
 @app.route('/')
 def daily_route():
-	html = render_template("accordian.html")
-	return html
+    html = render_template("accordian.html")
+    return html
 
 @app.route('/json')
 def json_example():
-	html = render_template("json.html")
-	return html
-
-@app.route('/define')
-def define_health_alerts():
-    html = render_template("define_health_alerts.html")
+    html = render_template("json.html")
     return html
+
+@app.route('/define_health_alert', methods=["GET"])
+def show_health_alerts():
+    health_alert_list = model.session.query(model.Health_Alert).order_by(model.Health_Alert.description).all()
+    return render_template("define_health_alerts.html", health_alert_list=health_alert_list)
+
+@app.route('/add_health_alert', methods=["POST"])
+def get_health_alert(): 
+    form_health_alert_description = request.form['health_alert_description']
+
+    new_health_alert = model.Health_Alert(
+        description = form_health_alert_description,
+        status = "active"
+        )
+
+    model.session.add(new_health_alert)
+    model.session.commit()
+    
+    return str(new_health_alert.id)
+
+@app.route('/delete_health_alert', methods=["POST"])
+def delete_health_alert():
+    form_health_alert = request.form["health_alert_id"]
+    model.session.query(model.Health_Alert).filter_by(id=form_health_alert).delete()
+    model.session.commit()
+    return "success"
 
 @app.route('/health')
 def health_alerts():
     html = render_template("health_alerts.html")
     return html
 
+@app.route('/participant')
+def show_participants():
+    participant_list = model.session.query(model.Participant).order_by(model.Participant.full_name).all()
+    return render_template("participant.html", participant_list=participant_list)
+
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
---
+++ model.py
@@ -0,0 +1,46 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Date
+
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
+import os
+
+db_uri = os.environ.get("DATABASE_URL", "sqlite:///rt.db")
+    
+engine = create_engine(db_uri, echo=False) 
+session = scoped_session(sessionmaker(bind=engine,
+                         autocommit = False,
+                         autoflush = False))
+
+Base = declarative_base()
+Base.query = session.query_property
+
+class Health_Alert(Base):
+    __tablename__ = "health_alerts"
+    id = Column(Integer, primary_key = True)
+    description = Column(String(128), nullable=False)
+    status = Column(String(256))
+
+class Participant(Base):
+    __tablename__ = "participants"
+    id = Column(Integer, primary_key = True)
+    full_name = Column(String(128), nullable=False)
+    status = Column(String(256))
+
+def create_db():
+    Base.metadata.create_all(engine)
+
+def connect(db_uri="sqlite:///ratings.db"):
+    global engine
+    global session
+    engine = create_engine(db_uri, echo=False) 
+    session = scoped_session(sessionmaker(bind=engine,
+                             autocommit = False,
+                             autoflush = False))
+
+def main():
+    """In case we need this for something"""
+    pass
+
+if __name__ == "__main__":
+    main()
